55db86006333a622e86e7074ae291586fedcc053
Daniel Ferketa <dangoita@gmail.com>
2015-06-12 11:19:13 -0700

    responding hound
    - reduced the line length

--- lib/pacecar/order.rb
+++ lib/pacecar/order.rb
@@ -14,7 +14,8 @@ module Pacecar
         safe_column_names.each do |name|
           scope "by_#{name}", ->(*args) {
             direction = args.flatten.first || 'asc'
-            order("#{quoted_table_name}.#{connection.quote_column_name name} #{direction}")
+            quoted_column_name = connection.quote_column_name name
+            order("#{quoted_table_name}.#{quoted_column_name} #{direction}")
           }
         end
       end
